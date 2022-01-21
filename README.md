1859eecd2d10cba2e7572e26b7c0cdc59331bc95
Michelle Wang <tianw0420@gmail.com>
2022-01-20 20:40:49 -0800

    choosing mark system

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Tic Tac Toe created for the Odin Project: https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/tic-tac-toe 
 
 To practice a modular Javascript organization 
+
+Things I learned: 
+-how to use nth child to select elements 
--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-let Gameboard = {
-    gameBoard: [], 
-    players: {}, 
-    displayController: {}
+const GameBoard = (() => {
+    let _board = [9]; 
+    
+    const getBoard = () => _board; 
+    const makeMove = (mark, index) => {
+        _board[index] = mark; 
+        let square = document.querySelector(`.gameboard:nth-child(${index + 1})`); 
+        square.textContent = mark; 
+    }; 
+
+    return {
+        getBoard, 
+        makeMove, 
+    }; 
+})(); 
+
+const Player = () => {
+    let _mark = ''; 
+    const chooseMark = () => {
+        if (document.getElementById('x-button').checked) _mark = 'X'; 
+        else _mark = 'O'; 
+    }; 
+    const getMark = () => _mark; 
+    const log = () => console.log('hi'); 
+    return {
+        log,
+        chooseMark, 
+        getMark
+    }; 
 }; 
+
+const GamePlay = (() => {
+    let player1 = Player(); 
+    let player2 = Player(); 
+    return {
+        player1, 
+        player2 
+    }; 
+})(); 
+
+const DisplayController = (() => {
+    const submitButton = document.querySelector('.submit-button'); 
+    submitButton.addEventListener('click', () => GamePlay.player1.chooseMark()); 
+})(); 
