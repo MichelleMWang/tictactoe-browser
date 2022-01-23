985b8dfd62f14ccf7c2cb58c20e5560043115b03
Michelle Wang <tianw0420@gmail.com>
2022-01-22 19:11:03 -0800

    added functions (except for checking for wins and what happens when wins happen)

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const GameBoard = (() => {
-    let _board = [9]; 
+    let _board = []; 
     
     const getBoard = () => _board; 
     const makeMove = (mark, index) => {
-        _board[index] = mark; 
-        let square = document.querySelector(`.gameboard:nth-child(${index + 1})`); 
-        square.textContent = mark; 
+        if (!_board[index]){
+            _board[index] = mark; 
+            let square = document.getElementById(index);
+            square.textContent = mark; 
+            GamePlay.changeTurns(); 
+        }
     }; 
 
     return {
@@ -14,24 +17,24 @@ const GameBoard = (() => {
     }; 
 })(); 
 
-const Player = () => {
+//factories are like classes, modules are like static classes
+const Player = (id) => {
+    let _id = id; 
     let _mark = ''; 
     let _score = 0; 
     const chooseMark = () => {
-        if (document.getElementById('x-button').checked) {
+        if (document.getElementById(`p${_id}-x-button`).checked) {
             _mark = 'X'; 
             return true; 
         }
-        else if (document.getElementById('o-button').checked){
+        else if (document.getElementById(`p${_id}-o-button`).checked){
             _mark = 'O'; 
             return true; 
         } else return false; 
     }; 
     const getMark = () => _mark; 
     const getScore = () => _score; 
-    const log = () => console.log('hi'); 
     return {
-        log,
         chooseMark, 
         getMark, 
         getScore
@@ -39,36 +42,69 @@ const Player = () => {
 }; 
 
 const GamePlay = (() => {
-    let player1 = Player(); 
-    let player2 = Player(); 
+    let player1 = Player(1); 
+    let player2 = Player(2); 
+    let turns = 0; 
+    let turn = player1; 
+
+    const changeTurns = () => {
+        turns++; 
+        if (turns % 2 == 0) turn = player1; 
+        else turn = player2; 
+    }
+    const getTurn = () => turn; 
+    const getTurns = () => turns; 
+
     return {
         player1, 
-        player2 
+        player2, 
+        getTurn, 
+        getTurns,
+        changeTurns
     }; 
 })(); 
 
 const DisplayController = (() => {
-    const chooseMark = () => {
-        const submitButton = document.querySelector('.submit-button'); 
-        let chooseSection = document.querySelector('.choose-mark'); 
+    const submitButton = document.querySelector('.submit-button'); 
+    let chooseSection = document.querySelector('.second-section'); 
+    let scoresSection = document.querySelector('.scores-section'); 
+    let gameBoard = document.querySelector('.game'); 
         
-        button.addEventListener('click', () => {
-            if (GamePlay.player1.chooseMark()){ 
+    submitButton.addEventListener('click', () => {
+        if (GamePlay.player1.chooseMark() && GamePlay.player2.chooseMark() ){ 
+            GamePlay.player1.chooseMark(); 
+            GamePlay.player2.chooseMark();  
+            initializeGame();                   
+            
+        } else {
+            let warning = document.createElement('p')
+            warning.classList.add('warning'); 
+            warning.textContent = 'please select a mark'; 
+            chooseSection.appendChild(warning); 
+        }        
+    }); 
 
-                chooseSection.classList.add('hide');
-            } else {
-                let warning = document.createElement('p')
-                warning.classList.add('warning'); 
-                warning.textContent = 'please select a mark'; 
-                chooseSection.appendChild(warning); 
-            }        
+    const initializeGame = () => {
+        chooseSection.classList.add('hide'); 
+        scoresSection.classList.toggle('hide');                    
+        gameBoard.classList.toggle('hide');
+        
+        let boardBoxes = document.querySelectorAll('.board-square'); 
+        boardBoxes.forEach(box => {
+            box.addEventListener('click', () => {
+                let id = box.id; 
+                let mark = GamePlay.getTurn().getMark(); 
+                GameBoard.makeMove(mark, id); 
+            }); 
         }); 
+
     }
     const setScores = () => {
-
+        
     }
 
     return {
-        chooseMark
+        initializeGame, 
+        setScores
     }
 })(); 
